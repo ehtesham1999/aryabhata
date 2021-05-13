5d5d04dfc0e7bb9d866c2735c7f0af7191bcb4dd
ehtesham1999 <ehteshamhussain1999@gmail.com>
2021-05-13 14:29:57 +0530

    heroku

--- server.js
+++ server.js
@@ -23,18 +23,19 @@ const connection = async () => {
       console.log("DB Connection Failed");
     }
   };
-// mongoose.connect('mongodb+srv://admin_ehte:test123@cluster0.tmjbb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true })
-// const db = mongoose.connection
-// db.on('error', (error) => console.error(error))
-// db.once('open', () => console.log('Connected to Database'))
 
 connection();
+// app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
 app.use(express.json());
 app.use(cors());
-process.env.PWD = process.cwd();
-app.get('/', function (req, res) {
-  res.send(process.env.PWD)
-})
+// process.env.PWD = process.cwd();
+// app.get('/', function (req, res) {
+//   res.send(process.env.PWD)
+// })
 const itemRoutes = require("./routes/itemRoutes");
 //requests targeting all items - get, post, delete
 app.use("/items/", itemRoutes);
