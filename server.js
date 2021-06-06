b1b6ecb1f2d62f5be20740fbfc89552239333f4e
ehtesham1999 <ehteshamhussain1999@gmail.com>
2021-06-06 15:32:43 +0530

    sdfsdf

--- server.js
+++ server.js
@@ -55,9 +55,9 @@ if(process.env.NODE_ENV === 'production')
   });
 }
 
-app.use(express.static(path.join(__dirname, "/client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build'));
-});
+// app.use(express.static(path.join(__dirname, "/client/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build'));
+// });
 
 app.listen(process.env.PORT || 5000, () => console.log("Server Started"));
