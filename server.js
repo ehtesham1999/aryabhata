a30ee80871ec35056049826c5666dd5eb8b45761
ehtesham1999 <ehteshamhussain1999@gmail.com>
2021-06-08 11:54:21 +0530

    heroku

--- server.js
+++ server.js
@@ -42,18 +42,18 @@ app.use('/items/', itemRoutes);
 app.use('/customers/', customerRoutes);
 app.use('/invoice/', invoiceRoutes);
 
-// app.use(express.static("client/build"));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
-// });
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+});
 
-if(process.env.NODE_ENV === 'production')
-{
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
-  });
-}
+// if(process.env.NODE_ENV === 'production')
+// {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+//   });
+// }
 
 // app.use(express.static(path.join(__dirname, "/client/build")));
 // app.get("*", (req, res) => {
